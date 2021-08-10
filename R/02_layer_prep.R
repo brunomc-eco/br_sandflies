4b59ddf3ab0c5ae21a278fa24bc09522f7f66203
brunomc-eco <brunomc.eco@gmail.com>
2021-08-10 17:45:24 +0200

    trying to add MOP

--- R/02_layer_prep.R
+++ R/02_layer_prep.R
@@ -6,6 +6,7 @@ library(raster)
 library(dplyr)
 library(readr)
 library(caret)
+library(kuenm)
 
 
 # define study extent -----------------------------------------------------
@@ -53,7 +54,54 @@ exclude_vars <- findCorrelation(cor(vals, method = 'spearman'),
 # selecting variables with lower correlation
 wc_sel <- wc[[which(!names(wc) %in% exclude_vars)]]
 
-names(wc_sel)
+wc_sel_names <- names(wc_sel)
 
 # save txt with selected variable names
-write_lines(names(wc_sel), file = "./outputs/02_selected_variable_names.txt")
+write_lines(wc_sel_names, file = "./outputs/02_selected_variable_names.txt")
+
+
+# MOP analysis ------------------------------------------------------------
+
+gcm_folder <- c("C:/layers/raster/worldclim2_cmip6_2.5/")
+
+# scenario and gcm names
+gcm_names <- c("BCC-CSM2-MR", "CanESM5", "CNRM-CM6-1", "CNRM-ESM2-1",
+               "IPSL-CM6A-LR", "MIROC6", "MIROC-ES2L", "MRI-ESM2-0")
+
+ssp_names <- c("ssp126_2041-2060", "ssp245_2041-2060", "ssp585_2041-2060")
+
+wc_sel <- wc_sel %>%
+  crop(ext)
+
+
+for(g in 1:length(ssp_names)){
+  
+  for(h in 1:length(gcm_names)){
+    
+    pred_gcm <- stack(paste0(gcm_folder, "wc2.1_2.5m_bioc_", gcm_names[h], "_", ssp_names[g], ".tif"))
+    
+    names(pred_gcm) <- c("wc2.1_2.5m_bio_1", "wc2.1_2.5m_bio_2", "wc2.1_2.5m_bio_3",
+                         "wc2.1_2.5m_bio_4", "wc2.1_2.5m_bio_5", "wc2.1_2.5m_bio_6",
+                         "wc2.1_2.5m_bio_7", "wc2.1_2.5m_bio_8", "wc2.1_2.5m_bio_9",
+                         "wc2.1_2.5m_bio_10", "wc2.1_2.5m_bio_11", "wc2.1_2.5m_bio_12",
+                         "wc2.1_2.5m_bio_13", "wc2.1_2.5m_bio_14", "wc2.1_2.5m_bio_15",
+                         "wc2.1_2.5m_bio_16", "wc2.1_2.5m_bio_17", "wc2.1_2.5m_bio_18",
+                         "wc2.1_2.5m_bio_19")
+    
+    pred_gcm <- subset(pred_gcm, wc_sel_names) %>%
+      crop(ext)
+    
+    mop_layer <- kuenm::kuenm_mop(M.variables = wc_sel, G.stack = pred_gcm, percent = 5,
+                                  parallel = TRUE, n.cores = 3)
+    
+    #mop_layer <- kuenm_mmop(G.var.dir = "C:/layers/raster/worldclim2_testing_future_SA_10/",
+    #                        M.var.dir = "C:/layers/raster/worldclim2_testing_prec_SA_10/",
+    #                        is.swd = FALSE,
+    #                        sets.var = c("prec_01_SA.tif", "prec_02_SA.tif", "prec_03_SA.tif"),
+    #                        percent = 5,
+    #                        out.mop = "C:/layers/raster/mop_test/")
+    
+  }
+  
+}
+
--- R/07_area_calculations.R
+++ R/07_area_calculations.R
@@ -1,23 +1,64 @@
-# Area calculations
+# ENM output area calculations
 # Bruno M. Carvalho
 # brunomc.eco@gmail.com
 
+library(readr)
+library(dplyr)
+library(raster)
 library(rgdal)
 
 
-# load data ---------------------------------------------------------------
+
+# load data and set values ------------------------------------------------
+
+# paths to layers
+layer_path <- c("C:/layers/raster/worldclim2_historical/")
+
+# selected predictors and extent
+
+wc_sel_names <- read_lines("./outputs/02_selected_variable_names.txt")
+ext <- readRDS("./outputs/02_study_extent.rds")
 
 
-## mask for cropping results to Brazil
+# species names
+study_sp <- c("L_neivai", "L_migonei", "L_longipalpis", "L_whitmani", 
+              "L_umbratilis", "L_flaviscutellata", "L_cruzi", 
+              "L_intermedia", "L_complexa", "L_wellcomei")
 
-br_mask <- readOGR("C:/layers/br/BRA_adm0.shp")
+# name the folder where previous final models were saved
+run_name <- c("./outputs/models_hist_100km/")
+
+# scenario and gcm names
+gcm_names <- c("BCC-CSM2-MR", "CanESM5", "CNRM-CM6-1", "CNRM-ESM2-1",
+               "IPSL-CM6A-LR", "MIROC6", "MIROC-ES2L", "MRI-ESM2-0")
+
+ssp_names <- c("ssp126_2041-2060", "ssp245_2041-2060", "ssp585_2041-2060")
+
+# mask for cropping results to Brazil
+br_mask <- readOGR("C:/layers/vector/GADM_Continental South America/BRA_adm/BRA_adm0.shp")
 
 
-## load model outputs
+
+
 
 
 
 # crop and calculate area -------------------------------------------------
+
+
+for(i in 1:length(study_sp)){
+  
+  ## load model outputs
+  
+  present <- list.files(path = paste0(run_name, study_sp[i], "/present/ensemble"),
+                        pattern = "consensus",
+                        full.names = TRUE) %>%
+    raster() %>%
+    mask(br_mask)
+  
+  plot(present)
+}
+
 
 
 
