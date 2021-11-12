00749ae3653f4fd467b03ac56ba64579d1ae533d
brunomc-eco <brunomc.eco@gmail.com>
2021-11-12 18:48:39 +0100

    adding categorical diffs

--- R/07_postprocessing.R
+++ R/07_postprocessing.R
@@ -18,7 +18,6 @@ run_name <- c("./outputs/models_chelsa_100km/")
 gcm_names <- c("GFDL-ESM4", "IPSL-CM6A-LR", "MPI-ESM1-2-HR", "MRI-ESM2-0", "UKESM1-0-LL")
 ssp_names <- c("ssp126", "ssp370", "ssp585")
 
-
 # species names
 study_sp <- c("L_complexa", "L_cruzi", "L_flaviscutellata", "L_intermedia",
               "L_longipalpis", "L_migonei", "L_neivai", "L_umbratilis",
@@ -28,38 +27,34 @@ study_sp <- c("L_complexa", "L_cruzi", "L_flaviscutellata", "L_intermedia",
 # model calibration area by species
 calib_files <- list.files("./data/shp/calib", pattern = ".shp", full.names = TRUE)
 
-# admin1 shapefile for mapping
-admin1 <- readOGR("C:/layers/vector/GADM/GADM_Continental South America/AmSulNew/AmSulNew.shp")
+# admin0 shapefile for mini-maps
+admin0 <- readOGR("C:/layers/vector/GADM/GADM_Continental South America/AmSulNew/AmSulNew.shp")
 
 
 # historical outputs ------------------------------------------------------
-
-# reclass matrix for final outputs - 0s to NAs to facilitate count and plot
-rclmat <- matrix(c(0, NA), ncol=2, byrow = TRUE)
 
 for(i in 1:length(study_sp)){
   
   # load historical final ensemble
   hist_mean <- raster(paste0(run_name, study_sp[i], "/present/ensemble/", study_sp[i], 
                              "_weighted_mean_ensemble.tif")) %>%
-    mask(readOGR(calib_files[i]))
+    mask(readOGR(calib_files[i]), updatevalue = 0)
   
   png(filename = paste0(run_name, study_sp[i], "/present/ensemble/", study_sp[i], "_mini_ensemble_mean.png"),
       res = 100)
-  plot(hist_mean, main = paste(study_sp[i]), "historical (1981-2010)")
-  plot(admin1, add=T)
+  plot(hist_mean, main = paste(study_sp[i], "historical (1981-2010)"))
+  plot(admin0, add=T)
   dev.off()
   
   hist_bin <- raster(paste0(run_name, study_sp[i], "/present/ensemble/", study_sp[i], 
                             "_consensus_ensemble.tif")) %>%
-    mask(readOGR(calib_files[i])) %>%
-    reclassify(rclmat)
+    mask(readOGR(calib_files[i]), updatevalue = 0) 
   
   png(filename = paste0(run_name, study_sp[i], "/present/ensemble/", study_sp[i], "_mini_ensemble_consensus.png"),
       res = 100)
-  plot(hist_bin, main = paste(study_sp[i], "historical (1981-2010)"), legend = FALSE, col = "#00A600")
-  legend("topright", legend = c("Unsuitable", "Suitable"), fill = rev(terrain.colors(2)))
-  plot(admin1, add=T)
+  plot(hist_bin, main = paste(study_sp[i], "historical (1981-2010)"), legend = FALSE, col = rev(terrain.colors(2)))
+  legend("topright", legend = c("Unsuitable", "Suitable"), fill = rev(terrain.colors(2)), bty = "n")
+  plot(admin0, add=T)
   dev.off()
   
   writeRaster(hist_bin, filename = paste0(run_name, study_sp[i], "/present/ensemble/", study_sp[i],
@@ -71,7 +66,7 @@ for(i in 1:length(study_sp)){
 
 
 
-# suitability differences -------------------------------------------------
+# suitability diffs, continuous -------------------------------------------
 
 
 for(i in 1:length(study_sp)){
@@ -79,90 +74,68 @@ for(i in 1:length(study_sp)){
   # load historical ensemble
   hist_mean <- raster(paste0(run_name, study_sp[i], "/present/ensemble/", study_sp[i], 
                              "_weighted_mean_ensemble.tif"))
-  # load future ssps
   
-  ssp126 <- raster(paste0(run_name, study_sp[i], "/future_consensus/", study_sp[i], 
-                       "_ssp126_mean.tif"))
-  ssp370 <- raster(paste0(run_name, study_sp[i], "/future_consensus/", study_sp[i], 
-                          "_ssp370_mean.tif"))
-  ssp585 <- raster(paste0(run_name, study_sp[i], "/future_consensus/", study_sp[i], 
-                          "_ssp585_mean.tif"))
-  
-  # calculate differences in pixel values
-  
-  diff_ssp126 = ssp126 - hist_mean
-  diff_ssp370 = ssp370 - hist_mean
-  diff_ssp585 = ssp585 - hist_mean
-  diffs = stack(diff_ssp126, diff_ssp370, diff_ssp585)
-  
-  # save diff outputs
-  if (!file.exists(paste0(run_name, study_sp[i], "/diffs"))) {
-    dir.create(paste0(run_name, study_sp[i], "/diffs"))
-  }
-  
-  writeRaster(diff_ssp126, filename = paste0(run_name, study_sp[i], "/diffs/",
-                                             study_sp[i], "_diff_ssp126.tif"), 
-              overwrite = TRUE)
-  
-  writeRaster(diff_ssp370, filename = paste0(run_name, study_sp[i], "/diffs/",
-                                             study_sp[i], "_diff_ssp370.tif"), 
-              overwrite = TRUE)
-  
-  writeRaster(diff_ssp585, filename = paste0(run_name, study_sp[i], "/diffs/",
-                                             study_sp[i], "_diff_ssp585.tif"), 
-              overwrite = TRUE)
-  
-  # save diff minimaps
-  
+  # calculate diffs
+  diffs <- list()
   for(g in 1:length(ssp_names)){
-    png(filename = paste0(run_name, study_sp[i], "/diffs/", 
-                          study_sp[i], "_mini_diff_", ssp_names[g],".png"),
-        res = 100)
-    plot(diffs[[g]], main = paste(study_sp[i], "diff", ssp_names[g], "(2071–2100)"))
-    plot(admin1, add=T)
-    dev.off()
-  }
+    ssp <- raster(paste0(run_name, study_sp[i], "/future_consensus/", study_sp[i], 
+                         "_", ssp_names[g], "_mean.tif"))
+    diffs[[g]] <- ssp - hist_mean
     
+    if (!file.exists(paste0(run_name, study_sp[i], "/diffs"))) {
+      dir.create(paste0(run_name, study_sp[i], "/diffs"))
+    }
+    
+    # save tiff file
+    writeRaster(diffs[[g]], filename = paste0(run_name, study_sp[i], "/diffs/", study_sp[i],
+                                              "_diff_", ssp_names[g], ".tif"), 
+                overwrite = TRUE)
+  }
   
+  # pixel density plots
+  pixels <- tibble(scenario = c(rep("ssp126", ncell(diffs[[1]])),
+                                rep("ssp370", ncell(diffs[[2]])),
+                                rep("ssp585", ncell(diffs[[3]]))),
+                   values = c(getValues(diffs[[1]]),
+                              getValues(diffs[[2]]),
+                              getValues(diffs[[3]])))
   
-  ## pixel density plots
-  
-  #pixels <- tibble(id = seq(1:ncell(diff_ssp126)),
-  #                 diff_ssp126_vals = getValues(diff_ssp126),
-  #                 diff_ssp370_vals = getValues(diff_ssp370),
-  #                 diff_ssp585_vals = getValues(diff_ssp585))
-  
-  pixels <- tibble(scenario = c(rep("ssp126", ncell(diff_ssp126)),
-                                rep("ssp370", ncell(diff_ssp370)),
-                                rep("ssp585", ncell(diff_ssp585))),
-                   values = c(getValues(diff_ssp126),
-                              getValues(diff_ssp370),
-                              getValues(diff_ssp585)))
-  
-  #pixels_suit <- tibble(scenario = c(rep("historical", ncell(hist_mean)),
-  #                                   rep("ssp126", ncell(ssp126)),
-  #                                   rep("ssp370", ncell(ssp370)),
-  #                                   rep("ssp585", ncell(ssp585))),
-  #                 values = c(getValues(hist_mean),
-  #                            getValues(ssp126),
-  #                            getValues(ssp370),
-  #                            getValues(ssp585)))
-  
-  # plot density (histograms)
   ggplot(pixels, aes(x = values, fill = scenario)) +
     geom_density(alpha=.2) +
     xlim(c(-1,1)) +
     geom_vline(xintercept = 0, linetype="dotted", color = "black", size=1) +
     labs(title= paste(study_sp[i], "suitability diff"))
-  
-  #ggplot(pixels_suit, aes(x = values, fill = scenario)) +
-  #  geom_density(alpha=.2) +
-  #  xlim(c(0,1)) +
-  #  geom_vline(xintercept = 0.5, linetype="dotted", color = "black", size=1) +
-  #  labs(title= paste(study_sp[i], "suitability"))
-  
-  # save
-  ggsave(filename = paste0(run_name, study_sp[i], "/diffs/", study_sp[i], "_densityplot.tif"),
+    
+  ggsave(filename = paste0(run_name, study_sp[i], "/diffs/", study_sp[i], "_diff_densityplot.tif"),
          width = 4200, height = 1800, units = "px", device='tiff', dpi=300)
+  
 }
 
+
+
+# suitability diffs, binary -----------------------------------------------
+
+
+for(i in 1:length(study_sp)){
+  
+  # load historical binary
+  hist_bin <- raster(paste0(run_name, study_sp[i], "/present/ensemble/", study_sp[i], 
+                            "_final_consensus_ensemble.tif"))
+  
+  for(g in 1:length(ssp_names)){
+    ssp_bin <- raster(paste0(run_name, study_sp[i], "/future_consensus/", study_sp[i], 
+                            "_", ssp_names[g], "_consensus.tif"))
+    ssp_bin <- ssp_bin >= 3
+    
+    expansion <- ((ssp_bin - hist_bin) == 1) * 4
+    contraction <- ((hist_bin - ssp_bin) == 1) * 2
+    stable <- (hist_bin + ssp_bin) == 2 
+    
+    diff_cat = expansion + contraction + stable # values: 1 = stable, 2 = contraction, 4 = expansion
+    
+    writeRaster(diff_cat, filename = paste0(run_name, study_sp[i], "/diffs/", study_sp[i],
+                                            "_diff_categorical_", ssp_names[g], ".tif"), 
+                overwrite = TRUE)
+  }
+  
+}
